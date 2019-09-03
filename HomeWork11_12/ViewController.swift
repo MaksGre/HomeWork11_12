160d03ce16e0de8c2c9dcbca5fdebeef8010e118
Maksim <maks_gr@rambler.ru>
2019-09-03 08:50:10 +0400

    Initial Commit

---
+++ HomeWork11_12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork11_12
+//
+//  Created by Maksim Grebenozhko on 03/09/2019.
+//  Copyright © 2019 Maksim Grebenozhko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
